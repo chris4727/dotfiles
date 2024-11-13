4c5287b712053a741df45ec6917178050ea9484e
Layka47 <chris47.27@proton.me>
2024-11-13 06:52:29 -0800

    Nvim: Added Telescope fuzzy finder

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -26,6 +26,10 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true },
+    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    }
   },
   --
   -- colorscheme that will be used when installing plugins.
@@ -37,3 +41,10 @@ require("lazy").setup({
 -- Set colorscheme
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
+
+-- Telescope setup
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
