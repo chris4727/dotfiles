f40fe1a1e3a0cbab2deef2e9a4b198953704e0d4
Layka47 <chris47.27@proton.me>
2024-11-15 12:30:39 -0800

    Added treesitter

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -25,11 +25,12 @@ vim.g.maplocalleader = "\\"
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true },
-    {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+	  { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true },
+	  {
+		  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		  dependencies = { 'nvim-lua/plenary.nvim' }
+	  },
+	  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }
   },
   --
   -- colorscheme that will be used when installing plugins.
@@ -42,9 +43,17 @@ require("lazy").setup({
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
 
--- Telescope setup
+-- Telescope config
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+---- Treesitter config
+--configs.setup({
+--	ensure_installed = { "lua", "vim", "vimdoc", "html" },
+--	sync_install = false,
+--	highlight = { enable = true },
+--	indent = { enable = true },  
+--})
