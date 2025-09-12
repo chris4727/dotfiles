0227ff29d837738bd754cee21252f8e417dfd502
chris4727 <123905042+chris4727@users.noreply.github.com>
2025-09-12 10:41:03 -0700

    qutebrowser: Init qutebrowser config.py with some default settings

---
+++ qutebrowser/.config/qutebrowser/config.py
@@ -0,0 +1,17 @@
+# Documentation:
+#   qute://help/configuring.html
+#   qute://help/settings.html
+
+# Change the argument to True to still load settings configured via autoconfig.yml
+config.load_autoconfig(False)
+
+# Which cookies to accept
+config.set('content.cookies.accept', 'all', 'chrome-devtools://*')
+# Which cookies to accept
+config.set('content.cookies.accept', 'all', 'devtools://*')
+# Valid values:
+#   - all: Accept all cookies.
+#   - no-3rdparty: Accept cookies from the same origin only. This is known to break some sites, such as GMail.
+#   - no-unknown-3rdparty: Accept cookies from the same origin only, unless a cookie is already set for the domain. On QtWebEngine, this is the same as no-3rdparty.
+#   - never: Don't accept cookies at all.
+
