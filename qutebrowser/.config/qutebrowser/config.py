6ef8ed40efa93ed5cb234f24dc05cd0399a9593f
chris4727 <123905042+chris4727@users.noreply.github.com>
2025-09-12 17:00:48 -0700

    qutebrowser: Updated theme

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -39,58 +39,86 @@ config.bind('v', 'hint links spawn --detach mpv {hint-url} ')
 
 # --- THEME --------------------------------------
 
-c.colors.statusbar.normal.bg = "#00000000"
-c.colors.statusbar.command.bg = "#00000000"
-c.colors.statusbar.command.fg = "#FFFFFFFF"
-c.colors.statusbar.normal.fg = "#FFFFFFFF"
-c.colors.statusbar.passthrough.fg = "#FFFFFFFF"
-c.colors.statusbar.url.fg = "#FFFFFFFF"
-c.colors.statusbar.url.success.https.fg = "#FFFFFFFF"
-c.colors.statusbar.url.hover.fg = "#FFFFFFFF"
-# c.statusbar.show = "always"
-c.colors.tabs.even.bg = "#00000000"
-c.colors.tabs.odd.bg = "#00000000"
-c.colors.tabs.bar.bg = "#00000000"
-# c.colors.tabs.even.bg = "#00000000"
-# c.colors.tabs.odd.bg = "#00000000"
-c.colors.tabs.even.fg = "#FFFFFFFF"
-c.colors.tabs.odd.fg = "#FFFFFFFF"
-c.colors.tabs.selected.even.bg = "#00000000"
-c.colors.tabs.selected.odd.bg = "#00000000"
-c.colors.tabs.selected.even.fg = "#FFFFFFFF"
-c.colors.tabs.selected.odd.fg = "#FFFFFFFF"
-c.colors.hints.bg = "#00000000"
-c.colors.hints.fg = "#FFFFFFFF"
-c.tabs.show = "multiple"
+palette = {
+            "bg0": "#191724",
+            "bg1": "#1f1d2e",
+            "bg2": "#26233a",
+            "fg2": "#6e6a86",
+            "fg1": "#908caa",
+            "fg0": "#e0def4",
+            "red": "#eb6f92",
+            "yel": "#f6c177",
+            "grn": "#9ccfd8",
+            "aqu": "#ebbcba",
+            "blu": "#31748f",
+            "prp": "#c4a7e7",
+            "tst": "#FF0000",
+}
+# Dark mode
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.bg = palette["bg0"]
+# Status bar
+c.colors.statusbar.normal.bg = palette["bg0"]
+c.colors.statusbar.normal.fg = palette["fg2"]
+c.colors.statusbar.command.bg = palette["bg0"]
+c.colors.statusbar.command.fg = palette["grn"]
+c.colors.statusbar.url.fg = palette["fg2"]
+c.colors.statusbar.url.success.https.fg = palette["grn"]
+c.colors.statusbar.url.hover.fg = palette["yel"]
+# Tabs
+c.colors.tabs.even.bg = palette["bg0"]
+c.colors.tabs.even.fg = palette["fg0"]
+c.colors.tabs.odd.bg = palette["bg0"]
+c.colors.tabs.odd.fg = palette["fg0"]
+c.colors.tabs.bar.bg = palette["bg0"]
+c.colors.tabs.selected.even.bg = palette["prp"]
+c.colors.tabs.selected.odd.bg = palette["prp"]
+c.colors.tabs.selected.even.fg = palette["bg0"]
+c.colors.tabs.selected.odd.fg = palette["bg0"]
+# Hints (when pressing "f" or "F")
+c.colors.hints.bg = palette["bg0"]
+c.colors.hints.fg = palette["fg0"]
+c.hints.border = "1px solid" + palette["fg2"]
+# Tab Indicator (the vertical bar on left of tabs)
+c.tabs.indicator.width = 13 # 0 = no tab indicator
+c.colors.tabs.indicator.start = palette["tst"]
+c.colors.tabs.indicator.stop = palette["grn"]
+# Completion widget (when typing commands)
+c.colors.completion.match.fg = palette["aqu"]
+c.colors.completion.odd.bg = palette["bg0"]
+c.colors.completion.even.bg = palette["bg0"]
+c.colors.completion.fg = palette["fg0"]
+c.colors.completion.category.bg = palette["bg0"]
+c.colors.completion.category.fg = palette["fg0"]
+c.colors.completion.item.selected.bg = palette["bg2"]
+c.colors.completion.item.selected.fg = palette["fg0"]
+c.colors.completion.item.selected.match.fg = palette["grn"]
+c.colors.completion.item.selected.border.top = palette["bg0"]
+c.colors.completion.item.selected.border.bottom = palette["bg0"]
+# Messages
+c.messages.timeout = 10000
+c.colors.messages.info.bg = palette["bg0"]
+c.colors.messages.info.fg = palette["yel"]
+c.colors.messages.info.border = palette["bg0"]
+c.colors.messages.error.bg = palette["bg0"]
+c.colors.messages.error.fg = palette["red"]
+c.colors.messages.error.border = palette["bg0"]
 
-c.colors.completion.item.selected.match.fg = "#FFFFFFFF"
-c.colors.completion.match.fg = "#FFFFFFFF"
+# Downloads
+c.colors.downloads.bar.bg = palette["bg0"]
+c.colors.downloads.start.bg = palette["bg0"]
+c.colors.downloads.stop.bg = palette["bg0"]
+c.colors.downloads.start.fg = palette["fg0"]
+c.colors.downloads.stop.fg = palette["fg0"]
+c.colors.downloads.error.bg = palette["bg0"]
+c.colors.downloads.error.fg = palette["red"]
 
-c.colors.tabs.indicator.start = ""
-c.colors.tabs.indicator.stop = ""
-c.colors.completion.odd.bg = "#00000000"
-c.colors.completion.even.bg = "#00000000"
-c.colors.completion.fg = "#FFFFFFFF"
-c.colors.completion.category.bg = "#00000000"
-c.colors.completion.category.fg = "#FFFFFFFF"
-c.colors.completion.item.selected.bg = "#00000000"
-c.colors.completion.item.selected.fg = "#FFFFFFFF"
+c.colors.prompts.bg = palette["bg1"]
+c.colors.prompts.fg = palette["fg0"]
+c.colors.prompts.selected.bg = palette["prp"]
+c.colors.prompts.selected.fg = palette["bg0"]
+c.colors.prompts.border = palette["tst"]
 
-c.colors.messages.info.bg = "#00000000"
-c.colors.messages.info.fg = "#FFFFFFFF"
-c.colors.messages.error.bg = "#00000000"
-c.colors.messages.error.fg = "#FFFFFFFF"
-c.colors.downloads.error.bg = "#00000000"
-c.colors.downloads.error.fg = "#FFFFFFFF"
-
-c.colors.downloads.bar.bg = "#00000000"
-c.colors.downloads.start.bg = "#00000000"
-c.colors.downloads.start.fg = "#FFFFFFFF"
-c.colors.downloads.stop.bg = "#00000000"
-c.colors.downloads.stop.fg = "#FFFFFFFF"
-
-c.colors.tooltip.bg = "#00000000"
-c.colors.webpage.bg = "#00000000"
-c.hints.border = ""
+c.colors.tooltip.bg = palette["bg0"]
 
 
